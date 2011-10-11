11a20acf512f2589fbe70959b71696f0d1aab1ce
Thomas Sutton <me@thomas-sutton.id.au>
2011-10-11 16:58:30 +0800

    Re-indent with tabs

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,21 +1,21 @@
 
 class collectd::params {
 
-  #
-  # Configuration file locations
-  #
-  
-  $collectd_conf = "/etc/collectd/collectd.conf"
-  
-  $collection_conf = "/etc/collectd/collection.conf"
-  
-  $filters_conf = "/etc/collectd/filters.conf"
-  
-  $thresholds_conf = "/etc/collectd/thresholds.conf"
-  
-  $password_file = "/etc/collectd/passwd"
+	#
+	# Configuration file locations
+	#
 
-  $plugins = [syslog, cpu, df, disk, entropy, interface, load, memory, process, swap, uptime, users, vmem]
+	$collectd_conf = "/etc/collectd/collectd.conf"
+
+	$collection_conf = "/etc/collectd/collection.conf"
+
+	$filters_conf = "/etc/collectd/filters.conf"
+
+	$thresholds_conf = "/etc/collectd/thresholds.conf"
+
+	$password_file = "/etc/collectd/passwd"
+
+	$plugins = [syslog, cpu, df, disk, entropy, interface, load, memory, process, swap, uptime, users, vmem]
 
 }
 
